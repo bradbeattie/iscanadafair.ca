39590cbdee421125e0bdac392bfa9f7e23dea494
Brad Beattie <bradbeattie@gmail.com>
2017-07-01 22:05:23 -0700

    Notebook fixes

--- compile.py
+++ compile.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 import os
+import shutil
 
 env = Environment(
     loader=FileSystemLoader(["templates", "content"]),
@@ -10,5 +11,12 @@ env = Environment(
 FileSystemLoader("..")
 for filename in os.listdir("content"):
     if not filename.endswith(".swp"):
-        with open(os.path.join("www", filename), "w") as f:
-            f.write(env.get_template(filename).render())
+        with open(os.path.join("content", filename)) as f:
+            if f.readline().startswith("{% extends"):
+                with open(os.path.join("www", filename), "w") as f:
+                    f.write(env.get_template(filename).render())
+            else:
+                shutil.copyfile(
+                    os.path.join("content", filename),
+                    os.path.join("www", filename),
+                )
