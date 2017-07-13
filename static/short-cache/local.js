0712cfae04086ba62e31575ef5bc1af1a781b522
Brad Beattie <bradbeattie@gmail.com>
2017-07-12 21:32:38 -0700

    Homepage adjustments

--- static/short-cache/local.js
+++ static/short-cache/local.js
@@ -1,6 +1,7 @@
 $("[data-reveal-after]").addClass("reveal")
 $(document).on("click", "button[data-reveal-trigger]", function() {
     var trigger = $(this).data("reveal-trigger")
+    $(this).parents(".jumbotron").removeClass("jumbotron").addClass("well")
     $("[data-reveal-trigger="+trigger+"]").remove()
     $("[data-reveal-after="+trigger+"]").addClass("revealed")
     $.post("?" + trigger, function(data) {});
